17fef70aae1dd382d3769907163f2de5d862609e
Philip Iezzi <philip@onlime.ch>
2023-08-24 21:38:39 +0200

    added LOG_MAIL facility to syslog logger parsing of boolean env variables in Config.get(), if provided default is a bool

--- app/conf.py
+++ app/conf.py
@@ -1,13 +1,18 @@
 from os import getenv
 from dotenv import load_dotenv
-
+from distutils.util import strtobool
+from typing import Any
 
 class Config:
     def __init__(self, path = None) -> None:
         load_dotenv(dotenv_path=path)
 
-    def get(self, key: str, default: object = None) -> str:
-        return getenv(key, default)
+    def get(self, key: str, default: Any = None) -> str:
+        value = getenv(key, default)
+        if type(default) is bool and type(value) is not bool:
+            return bool(strtobool(value))
+        else:
+            return value
 
-    def get_array(self, key: str, default: object = None) -> list:
+    def get_array(self, key: str, default: Any = None) -> list:
         return getenv(key, default).split(',')
--- app/logging.py
+++ app/logging.py
@@ -1,17 +1,22 @@
 import logging
 import logging.handlers
+from app.conf import Config
 
-def get_logger(conf):
+def get_logger(conf: Config):
     """Get logger"""
     logger = logging.getLogger('policyd-rate-guard')
     logger.setLevel(getattr(logging, conf.get('LOG_LEVEL', 'INFO').upper(), logging.INFO))
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    if conf.get('SYSLOG', 'False').lower() in ('true', 'yes', '1'):
-        syslog = logging.handlers.SysLogHandler(address='/dev/log')
-        # TODO: Make facility configurable
+    if conf.get('SYSLOG', False):
+        formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+        syslog = logging.handlers.SysLogHandler(
+            # workaround for Docker container: address=('localhost', 514),
+            address='/dev/log',
+            facility=logging.handlers.SysLogHandler.LOG_MAIL,
+        )
         syslog.setFormatter(formatter)
         logger.addHandler(syslog)
     else:
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         console = logging.StreamHandler()
         console.setFormatter(formatter)
         logger.addHandler(console)
