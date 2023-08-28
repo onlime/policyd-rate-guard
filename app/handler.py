2485192e7ea813ee69298807c91bc3d34fb6cf58
Philip Iezzi <philip@onlime.ch>
2023-08-28 10:20:56 +0200

    workaround for

--- app/handler.py
+++ app/handler.py
@@ -3,7 +3,6 @@ from app.message import Message
 class Handler:
     """Handle request"""
 
-    request = {}
     data = ''
 
     def __init__(self, conn: object, addr: str, conf: object, logger: object, db: object):
@@ -22,45 +21,46 @@ class Handler:
 
     def handle(self):
         """Handle request"""
-
-        # PyMySQL: Ensure the db connection is alive
-        if self.conf.get('DB_DRIVER', 'pymysql').lower() == 'pymysql':
-            self.db.ping(reconnect=True)
-
         # Read data
         self.data = self.conn.recv(2048).decode('utf-8') # Attention: We only read first 2048 bytes, which is sufficient for our needs
         if not self.data:
             raise Exception('No data received')
         self.logger.debug('handler.py - Received data: %s', self.data)
+
         # Parse data
+        request = {}
         for line in self.data.split("\n"): # TODO: How to get subject, cc, bcc?
             line = line.strip()
             try:
                 key, value = line.split(u'=', 1)
                 if value:
                     self.logger.debug('handler.py - Received header: %s=%s', key, value)
-                    self.request[key] = value
+                    request[key] = value
             except ValueError: # Needed to ignore lines without "=" (e.g. the final two empty lines)
                 pass
 
         # Break no sasl_username was found (e.g. on incoming mail on port 25)
-        if 'sasl_username' not in self.request:
+        if 'sasl_username' not in request:
             self.logger.debug('handler.py - sasl_username is empty, accepting message and reply with DUNNO')
             self.send_response('DUNNO')
             self.conn.close()
             return
 
+        # PyMySQL: Ensure the db connection is alive
+        if self.conf.get('DB_DRIVER', 'pymysql').lower() == 'pymysql':
+            self.db.ping(reconnect=True)
+
         # Handle message
         message = Message(
-            msgid=self.request['queue_id'],
-            sender=self.request['sasl_username'] or '_NO_SASL_' + self.request['sender'],
-            client_address=self.request['client_address'],
-            client_name=self.request['client_name'],
-            rcpt_count=self.request['recipient_count'],
-            from_addr=self.request['sender'],
-            to_addr=self.request.get('recipient'),
-            cc_addr=self.request.get('cc_address'),
-            bcc_addr=self.request.get('bcc_address'),
+            msgid=request['queue_id'],
+            sender=request['sasl_username'],
+            client_address=request['client_address'],
+            client_name=request['client_name'],
+            rcpt_count=request['recipient_count'],
+            from_addr=request['sender'],
+            to_addr=request.get('recipient'),
+            cc_addr=request.get('cc_address'),
+            bcc_addr=request.get('bcc_address'),
             db=self.db,
             conf=self.conf,
             logger=self.logger
@@ -77,7 +77,7 @@ class Handler:
             message.msgid,
             message.client_name,
             message.client_address,
-            self.request['sasl_method'], # currently not stored in Message object or `messages` table
+            request['sasl_method'], # currently not stored in Message object or `messages` table
             message.sender,
             message.from_addr,
             message.rcpt_count,
